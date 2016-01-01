561fa798b2bbed32b311426469ca83f95827a3b5
naomooo <naomooo.official@gmail.com>
2016-01-01 15:57:14 +0900

    updateアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,12 +21,18 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    Prototype.update(create_params)
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(update_params)
+    redirect_to :root
   end
 
 
   private
   def create_params
     params.require(:prototype).permit(:title, :concept, :catchcopy, :tag1, :tag2, :tag3, thumbnails_attributes: [:image, :property]).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:prototype).permit(:title, :concept, :catchcopy, :tag1, :tag2, :tag3, thumbnails_attributes: [:image, :property, :id]).merge(user_id: current_user.id)
   end
 end
