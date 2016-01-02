d7383d70545f49bac0dd61448cca5f787d78bd2a
naomooo <naomooo.official@gmail.com>
2016-01-02 18:28:03 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231011439) do
+ActiveRecord::Schema.define(version: 20160102074233) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.text     "text",         limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+  end
+
+  add_index "comments", ["prototype_id"], name: "index_comments_on_prototype_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255,   null: false
@@ -63,6 +74,8 @@ ActiveRecord::Schema.define(version: 20151231011439) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
   add_foreign_key "thumbnails", "prototypes"
 end
