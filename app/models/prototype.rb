a36458d071ece3dc508cebb2550856beeceba4cc
naomooo <naomooo.official@gmail.com>
2016-01-03 11:50:24 +0900

    like機能に関するアソシエーションの追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :thumbnails, dependent: :delete_all
   has_many :comments
+  has_many :likes, dependent: :delete_all
 
   accepts_nested_attributes_for :thumbnails
   paginates_per 8
